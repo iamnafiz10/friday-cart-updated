a6a451a0e2cd2efb757f5860d2e07eb25c1b830f
Roshan SaiHam Nafiz <118830871+iamnafiz10@users.noreply.github.com>
2025-10-17 16:07:23 +0600

    Update again

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,9 +18,9 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <ClerkProvider
-            publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-            signInRedirectUrl  ="/"
-            signUpRedirectUrl ="/"
+            signInFallbackRedirectUrl="/"
+            signUpFallbackRedirectUrl="/"
+            appearance={{}}
         >
             <html lang="en">
             <body className={`${outfit.className} antialiased`}>
